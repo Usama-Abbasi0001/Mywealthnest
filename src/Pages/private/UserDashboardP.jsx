30c0948c43f319acf4d52a85c39449a9e1b5e521
Usama-Abbasi0001 <usamakhan58461@gmail.com>
2025-03-03 14:43:46 +0500

    offcanvs

--- src/Pages/private/UserDashboardP.jsx
+++ src/Pages/private/UserDashboardP.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import UserLeftOffCanva from '../../components/userDashboard/UserLeftOffCanva';
 import { Logo, UserDashboard } from '../../components/Utils/Icons';
+import DashboardOne from '../../components/userDashboard/DashboardOne';
+import { useNavigate } from 'react-router-dom';
+
 function UserDashboardP() {
+  const navigate = useNavigate();
+  function login(){
+    navigate("/login")
+  }
   return (
+    <>
     <div className='w-full flex justify-between items-center bg-gray-800 p-3'>
-      <div className='flex space-x-4 items-center'>
+      <div className='flex space-x-2 items-center'>
         <UserLeftOffCanva/>
-        <Logo/>
+       <div  ><Logo/> </div> 
       </div>
-      <div className=''>
+      <div className='' onClick={login}>
         <UserDashboard/>
       </div>
     </div>
+    <DashboardOne/>
+    </>
 
   )
 }
